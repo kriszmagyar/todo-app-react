61eb4cfc85709c4d7b4005d109f9069773823552
Krisz Magyar <krisz.magyar@gmail.com>
2019-09-10 17:29:38 +0200

    connect app with api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/todos')
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          throw new Error(res.statusText);
+        }
+      } )
+      .then(res => setTodos(res))
+      .catch(err => console.error(err))
+  }, []);
+
   return (
     <div className="App">
-      Hello
+      <ul>
+        { todos.map(todo => 
+          <li key={todo.id}>{ todo.title }</li>
+        ) }
+      </ul>
     </div>
   );
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json());
 
 app.get('/api/ping', (req, res) => {
- return res.sendStatus(200);
+    return res.sendStatus(200);
 });
+
+app.get('/api/todos', (req, res) => {
+    return res.send([{ id: 1, title: 'Todo1' }, { id: 2, title: 'Todo2' }]);
+})
 
 app.listen(process.env.PORT || 8080);
 console.log('Server started on 8080');
