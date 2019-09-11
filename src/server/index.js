9b1ffc1560e0422987b405813a6636d4a406c041
Krisz Magyar <krisz.magyar@gmail.com>
2019-09-11 13:47:53 +0200

    support crud operations on server

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-let { todos } = require('./todos');
+let { todos, getNextId } = require('./todos');
 
-app.use(express.urlencoded());
+const SERVER_PORT = 8080;
+
 app.use(express.json());
 
 app.get('/api/todos', (req, res) => {
@@ -14,14 +15,43 @@ app.get('/api/todos/:id', (req, res) => {
     const id = +req.params.id;
     const todo = todos.find(t => t.id === id);
 
-    return typeof todo === 'undefined'
+    return !todo
         ? res.status(404).send(`Todo with id ${id} is not found!`)
         : res.send(todo);
 });
 
 app.post('/api/todos', (req, res) => {
-    console.log(req.body);
+    const { title } = req.body;
+
+    if (title) {
+        const newTodo = { id: getNextId(), title };
+        todos.push(newTodo);
+        res.location(`/api/todos/${newTodo.id}`);
+        return res.status(201).send(newTodo);
+    } else {
+        return res.status(400).send('Missing or wrong parameters!');
+    }
+});
+
+app.put('/api/todos/:id', (req, res) => {
+    const id = +req.params.id;
+    const { title } = req.body;
+    const todo = todos.find(t => t.id === id);
+
+    if (todo && title) {
+        todo.title = title;
+        return res.status(204).send();
+    } else {
+        return res.status(400).send("Invalid request!");
+    }
+});
+
+app.delete('/api/todos/:id', (req, res) => {
+    const id = +req.params.id;
+    todos = todos.filter(t => t.id !== id);
+    return res.status(204).send();
 })
 
-app.listen(process.env.PORT || 8080);
-console.log('Server started on 8080');
+app.listen(process.env.PORT || SERVER_PORT, () => {
+    console.log(`Server started on ${SERVER_PORT}.`);
+});
--- src/server/todos.js
+++ src/server/todos.js
@@ -1,4 +1,12 @@
-exports.todos = [
-    { id: 1, title: 'Todo1' }, 
-    { id: 2, title: 'Todo2' }
+let currentId = 1;
+const getNextId = () => {
+    return currentId++;
+}
+
+const todos = [
+    { id: getNextId(), title: 'Todo1' }, 
+    { id: getNextId(), title: 'Todo2' }
 ];
+
+exports.todos = todos;
+exports.getNextId = getNextId;
