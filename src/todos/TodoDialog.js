405c17550939338f48fb994912d2cd676663b110
Krisz Magyar <krisz.magyar@gmail.com>
2019-09-13 15:13:57 +0200

    add responsivity

--- src/todos/TodoDialog.js
+++ src/todos/TodoDialog.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle, useTheme, useMediaQuery } from '@material-ui/core';
 
 export default function TodoDialog({ open, handleClose, handleSubmit }) {
 
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" fullScreen={fullScreen}>
         <DialogTitle id="form-dialog-title">Todo</DialogTitle>
         <DialogContent>
           <TextField
